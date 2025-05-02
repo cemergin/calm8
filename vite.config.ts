1ab62a81c5b0d0a92cb75c95432acde8c76dcdfa
Cem Ergin <birazgecemuzigi@gmail.com>
2025-05-01 21:06:43 -0400

    package updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,35 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { crx } from 'vite-plugin-chrome-extension';
-import manifest from './public/manifest.json'; // (we'll create this next)
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  plugins: [react(), crx({ manifest })],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/*',
+          dest: '.',
+        },
+      ],
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
   build: {
+    rollupOptions: {
+      input: {
+        popup: resolve(__dirname, 'src/popup/popup.html'),
+        content: resolve(__dirname, 'src/content/content.ts'),
+      },
+      output: {
+        entryFileNames: '[name].js',
+      },
+    },
     outDir: 'dist',
     emptyOutDir: true,
   },
