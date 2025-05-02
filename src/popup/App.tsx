edb9dac105d2c676edee88d81d52f75dbca8eaf5
Cem Ergin <birazgecemuzigi@gmail.com>
2025-05-01 21:43:54 -0400

    Add React popup UI scaffold

---
+++ src/popup/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: '16px', fontFamily: 'Arial, sans-serif' }}>
+      <h1>EventSeek</h1>
+      <p>This will become your popup UI.</p>
+    </div>
+  );
+}
+
+export default App;
